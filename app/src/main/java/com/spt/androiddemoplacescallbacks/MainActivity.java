19f52794cb657a180316a2973bd545fb62cd04e4
Kevin Gilles <kevin@singlespot.com>
2021-03-04 11:54:07 +0100

    First Commit

---
+++ app/src/main/java/com/spt/androiddemoplacescallbacks/MainActivity.java
@@ -0,0 +1,59 @@
+package com.spt.androiddemoplacescallbacks;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.sptproximitykit.SPTProximityKit;
+import com.sptproximitykit.SPTTestPlacesCallbacks;
+import com.sptproximitykit.geodata.places.SPTPlaceCallbackConfig;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static SPTProximityKit.LocationRequestMode locMode = SPTProximityKit.LocationRequestMode.serverBased;
+    private static SPTProximityKit.CmpMode cmpMode = SPTProximityKit.CmpMode.atLaunch;
+
+    private final static double LATITUDE_LOUVRE = 49.0333;
+    private final static double LONGITUDE_LOUVRE = 2.5;
+
+    private final static double LATITUDE_EIFFEL_TOWER = 48.858370;
+    private final static double LONGITUDE_EIFFEL_TOWER = 2.294481;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initSinglespot(this);
+        SPTTestPlacesCallbacks.setHomePlaceAtLatLong(this, LATITUDE_LOUVRE, LONGITUDE_LOUVRE);
+        SPTTestPlacesCallbacks.setWorkPlaceAtLatLong(this, LATITUDE_EIFFEL_TOWER, LONGITUDE_EIFFEL_TOWER);
+    }
+
+    private void initSinglespot(final Activity activity) {
+        SPTProximityKit.init(activity, locMode, cmpMode);
+        setSinglespotPlacesCallback();
+        SPTProximityKit.registerPlaceBroadcastReceiver(activity, new PlaceCallbackReceiver());
+    }
+
+    /**
+     * If you do not receive the notifications, make sure you're not outside of the time slots defined in the parameters
+     */
+    private void setSinglespotPlacesCallback() {
+        // For this example, we allow the Home Broadcast to be sent only after 4pm or before 9am
+        // We also don't want a more than one trigger per two hour window and set it
+        // accordingly in the SPTPlaceCallbackConfig as shown below:
+        SPTPlaceCallbackConfig homeConfiguration = new SPTPlaceCallbackConfig(16, 9, 0);
+
+        // Simple second example for a Broadcast to be sent to the PlaceCallbackReceiver
+        // only between 9am and 5pm. Moreover, we don't want more than one broadcast per hour.
+        SPTPlaceCallbackConfig workConfiguration = new SPTPlaceCallbackConfig(9, 17, 0);
+
+        // Using the setEnterHomeCallback ensures the Broadcast to be received only upon entering
+        // the Home area, this won't be fired on exit.
+        SPTProximityKit.setEnterHomeCallback(this, homeConfiguration);
+
+        // Complete opposite to the first example, this Broadcast will only be fired when the device
+        // exits the Work area.
+        SPTProximityKit.setExitWorkCallback(this, workConfiguration);
+    }
+}
---
+++ app/src/main/java/com/spt/androiddemoplacescallbacks/PlaceCallbackReceiver.java
@@ -0,0 +1,62 @@
+package com.spt.androiddemoplacescallbacks;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import static com.sptproximitykit.geodata.places.SPTPlaceCallbackConfig.PlaceTransition;
+import static com.sptproximitykit.geodata.places.SPTPlaceCallbackConfig.PlaceType;
+import static com.sptproximitykit.geodata.places.SPTPlaceCallbackConfig.SPT_BROADCAST_TRANSITION_KEY;
+import static com.sptproximitykit.geodata.places.SPTPlaceCallbackConfig.SPT_BROADCAST_TYPE_KEY;
+
+public class PlaceCallbackReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (context == null || intent == null || intent.getExtras() == null) return;
+        PlaceTransition transition = (PlaceTransition) intent.getExtras().getSerializable(SPT_BROADCAST_TRANSITION_KEY);
+        PlaceType type = (PlaceType) intent.getExtras().getSerializable(SPT_BROADCAST_TYPE_KEY);
+        if (transition == PlaceTransition.ENTER && type == PlaceType.HOME)
+            homeNotification(context);
+        else if (transition == PlaceTransition.EXIT && type == PlaceType.WORK)
+            workNotification(context);
+    }
+
+    private void homeNotification(Context context) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "CHANNEL_ID")
+                .setContentTitle("Singlespot \uD83C\uDFE0")
+                .setContentText("Entering Home Place!")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setSmallIcon(R.mipmap.ic_launcher);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String channelId = "Your_channel_id";
+            NotificationChannel channel = new NotificationChannel(channelId, "Channel human readable title", NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(channel);
+            builder.setChannelId(channelId);
+        }
+        notificationManager.notify(12500, builder.build());
+    }
+
+    private void workNotification(Context context) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "CHANNEL_ID")
+                .setContentTitle("Singlespot \uD83C\uDFE2")
+                .setContentText("Exiting Workplace!")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setSmallIcon(R.mipmap.ic_launcher);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String channelId = "Your_channel_id";
+            NotificationChannel channel = new NotificationChannel(channelId, "Channel human readable title", NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(channel);
+            builder.setChannelId(channelId);
+        }
+        notificationManager.notify(12500, builder.build());
+    }
+}
